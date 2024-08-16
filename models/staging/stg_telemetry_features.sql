83d00e804044f7f6e16b8e62dfaa6a6e4aaa5d0c
Sahil Jain <jainsahil77@gmail.com>
2024-08-16 18:21:51 +0530

    Update stg_telemetry_features.sql

--- models/staging/stg_telemetry_features.sql
+++ models/staging/stg_telemetry_features.sql
@@ -2,7 +2,7 @@
 WITH TelemetryFeatures AS (
     SELECT
         t.DATETIME,
-        t."machineID",
+        t.machine_id,
         t.VOLT,
         t.ROTATE,
         t.PRESSURE,
@@ -10,8 +10,8 @@ WITH TelemetryFeatures AS (
         m.MODEL,
         m.AGE
     FROM
-        DBT_TEST.DBO."PdM_telemetry" t
-    JOIN DBT_TEST.DBO."PdM_machines" m ON t."machineID" = m."machineID"
+        DBT_TEST.DBO.pdm_telemetry t
+    JOIN DBT_TEST.DBO.pdm_machines m ON t.machine_id = m.machine_id
 )
 
 select * from TelemetryFeatures
